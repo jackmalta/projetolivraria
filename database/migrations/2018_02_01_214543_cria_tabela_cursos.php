8887e533b6ee8632deedeb4fe7ef5d5f67c182d8
php 09 <php.09@tom.cec.ime.usp.br>
2018-02-02 19:43:45 -0200

    projeto Livraria

---
+++ database/migrations/2018_02_01_214543_cria_tabela_cursos.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CriaTabelaCursos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cursos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char('turma');
+            $table->string('nome');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cursos');
+    }
+}
